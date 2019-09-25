1ea952994d5db3b46e8936a9e13721d10330d620
AlexSantana-Met <alejandro.sant12.12@gmail.com>
2019-09-24 21:42:31 -0500

    Primer push proyecto

---
+++ EcommerceProject/Controllers/ProductosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EcommerceProject.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Productos()
+        {
+            return View();
+        }
+
+        public ActionResult InfoProducto()
+        {
+            return View();
+        }
+    }
+}
